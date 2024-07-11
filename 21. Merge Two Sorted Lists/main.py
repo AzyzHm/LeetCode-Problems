6485511b1e6c68338e049922f9b0d466adb06833
Mohammed Aziz Hammemi <Elhammemi001@gmail.com>
2024-07-11 22:25:26 +0200

    fix

--- 21. Merge Two Sorted Lists/main.py
+++ 21. Merge Two Sorted Lists/main.py
@@ -1,18 +1,24 @@
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == nullptr) {
-            return list2;
-        }
-        if (list2 == nullptr) {
-            return list1;
-        }
-        if (list1->val < list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        } else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
-        }
-    }
-};
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(list1, list2):
+    if list1 is None:
+        return list2
+    if list2 is None:
+        return list1
+    if list1.val < list2.val:
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+
+def main():
+    # Example usage
+    # Create lists here and call mergeTwoLists
+    pass
+
+if __name__ == "__main__":
+    main()
